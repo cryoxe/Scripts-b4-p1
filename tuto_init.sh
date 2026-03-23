061e29f02b4f7dba563c123ae3cdfa5571ad8434
cryoxe <71180076+cryoxe@users.noreply.github.com>
2026-03-23 15:49:01 +0100

    updated tutorial clone link to http

--- tuto_init.sh
+++ tuto_init.sh
@@ -10,7 +10,7 @@ fi
 
 echo "Cloning tutorial..."
 
-git clone git@github.com:cryoxe/Tutorial-b4-p1.git tuto_git
+git clone https://github.com/cryoxe/Tutorial-b4-p1.git tuto_git
 
 echo "Download done."
 echo "Preparing repository..."
